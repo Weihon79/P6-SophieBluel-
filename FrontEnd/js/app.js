df04e3ebbc4dd15c91af79540a6fb67f65648312
Weihon79 <ervan.chin@gmail.com>
2024-12-14 16:50:14 +0100

    test modale, position, contenu , javascript

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -152,6 +152,30 @@ function editMode() {
       editLink.innerHTML = '<i class="fa-solid fa-pen-to-square"></i> modifier';
       sectionHeader.appendChild(editLink);
 
+      let modal = null;
+
+      const openModal = function (e) {
+        e.preventDefault();
+        const target = document.querySelector(e.target.getAttribute("href"));
+        target.style.display = null;
+        target.removeAttribute("aria-modal", "true");
+        modal = target;
+        modal.addEventListener("click", closeModal)
+      }
+
+      const closeModal = function (e) {
+        if (modal === null) return;
+        e.preventDefault();
+        modal.style.display = "none";
+        modal.setAttribute("aria-hidden", "true");
+        modal.removeAttribute("aria-modal");
+        modal.removeEventListener("click", closeModal);
+        modal = null;
+      }
+
+      document.querySelector(".edit-link");
+
+      editLink.addEventListener("click", openModal);
   }
 }
 /*---------------------------------------------------------------------------------------------*/
